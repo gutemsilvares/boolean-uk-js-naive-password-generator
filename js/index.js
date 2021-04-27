019f3a78be89ade63dd8b291bf251004102b7321
Agutembergues <gutemsilvares@hotmail.com>
2021-04-27 23:54:47 +0100

    init

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let firstName = prompt("what is your Name?").slice(1, 4);
+let lastName = prompt("What's your Last Name?").slice(2.6, 7);
+let favColor = prompt("What is your favorite color").slice(1, 2, 3);
+
+console.log(firstName + lastName + favColor + 42);
